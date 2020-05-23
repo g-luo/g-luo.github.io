8d6f23dd0983d4f52c41e59463a6ef58f311634e
Grace Luo <graceluo@berkeley.edu>
2020-05-22 17:23:51 -0700

    change cdn source

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,4 +1,4 @@
-import {LitElement, html} from 'https://unpkg.com/@polymer/lit-element/lit-element.js?module';
+import {LitElement, html} from 'https://cdn.pika.dev/lit-element';
 
 // Static navbar on the left side of every page
 export class Sidebar extends LitElement { 
